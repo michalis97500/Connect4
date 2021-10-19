9b3d1325c820965ca9afcff80f0f6da4503fbee3
Michalis Christou <Michalis.Christou@PerkinElmer.com>
2021-10-19 08:26:08 +0300

    working dumb AI

--- MyConnectFour.java
+++ MyConnectFour.java
@@ -11,12 +11,13 @@ public class MyConnectFour {
     try{
       addPlayers();
       playGame();
-    }catch(Exception e ){
+    }catch(Exception e){
     }
   }
 
   private void playGame() {
-    input.setMax(newBoard.boardX);
+    input.setMax(newBoard.getBoardX());
+    checkAIandStart();
     try{
       newBoard.printBoard();
       boolean win = false;
@@ -24,18 +25,27 @@ public class MyConnectFour {
       while (!win){
         int move = 999999;
         for(Player currentPlayer : players){
-          boolean validMove = false;
-          while(!validMove){
-            move = input.ReadLine();
-            if(move != 999999){//valid move
-              validMove = true;
+          if(currentPlayer.amIHuman() == true){
+            boolean validInput = false;
+            while(!validInput){
+              move = input.ReadLine();
+              if(move != 999999){//valid move
+                validInput = true;
+             }
             }
-          }
-          currentPlayer.myMove(move);
-          win = currentPlayer.haveIWon();
-          if(win==true){
-            winner = currentPlayer.getName();
-            break;
+            currentPlayer.myMove(move);
+            win = currentPlayer.haveIWon();
+            if(win==true){
+              winner = currentPlayer.getName();
+              break;
+            }
+          }else if(currentPlayer.amIHuman() != true){
+            currentPlayer.makeMove();
+            win = currentPlayer.haveIWon();
+            if(win==true){
+              winner = currentPlayer.getName();
+              break;
+            }
           }
         }
       }
@@ -45,8 +55,13 @@ public class MyConnectFour {
     }
   }
 
+  private void checkAIandStart(){
+    players.get(1).setEnemyCharacter(players.get(0).getChar());
+    players.get(0).setEnemyCharacter(players.get(1).getChar());
+  }
+
   private void addPlayers(){
-    System.out.println("Enter number of players (2-10)");
+    System.out.println("Enter number of players (2-10). If AI is to be used only 2 players allowed");
     int numberOfPlayers = input.ReadLine();
     try{
     while(numberOfPlayers<=1 || numberOfPlayers >10){
@@ -54,34 +69,47 @@ public class MyConnectFour {
       numberOfPlayers = input.ReadLine();
     }
     for(int i=0;i<numberOfPlayers;i++){
+      Player newPlayer;
       int z = 1+i;
       System.out.println("Enter name of player " + z);
       String newPlayerName = settings.readLine();
-      System.out.println("Is this player human? (Y or N)");
-      String playerHuman = settings.readLine();
       Boolean humanHelper = false, human = true, charHelper = false;
-      while (!humanHelper){
-        switch(playerHuman){
-          case "y":
-            human=true;
-            humanHelper=true;
-            break;
-          case "Y":
-            human=true;
-            humanHelper=true;
-            break;
-          case "N":
-            human=false;
-            humanHelper=true;
-            break;
-          case "n":
-            human=false;
-            humanHelper=true;
-            break;
-          default:
-            System.out.println("Enter Y or N only");
-            playerHuman = settings.readLine();
-            break;
+      if(numberOfPlayers==2){
+        System.out.println("Is this player human? (Y or N)");
+        String playerHuman = settings.readLine();
+        while (!humanHelper){
+          switch(playerHuman){
+            case "y":
+              human=true;
+              humanHelper=true;
+              break;
+            case "Y":
+              human=true;
+              humanHelper=true;
+              break;
+            case "N":
+              if(numberOfPlayers==2){
+                human=false;
+                humanHelper=true;
+                break;
+              }
+              human=true;
+              humanHelper=true;
+              break;
+            case "n":
+              if(numberOfPlayers==2){
+                human=false;
+                humanHelper=true;
+                break;
+              }
+              human=true;
+              humanHelper=true;
+              break;
+            default:
+              System.out.println("Enter Y or N only");
+              playerHuman = settings.readLine();
+              break;
+          }
         }
       }
       System.out.println("Enter a character for this player");
@@ -102,7 +130,11 @@ public class MyConnectFour {
           charHelper = true;
         }
       }
-      Player newPlayer = new Player(newPlayerName,newPlayerCharacter, newBoard);
+      if(human == true){
+        newPlayer = new Player(newPlayerName,newPlayerCharacter, newBoard,human);
+      }else{
+        newPlayer = new AI(newPlayerName,newPlayerCharacter, newBoard,1);
+      }
       players.add(newPlayer);
     }
     }catch(Exception e){
